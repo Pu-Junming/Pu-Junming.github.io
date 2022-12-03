813bac25ac7723c7cc1b58d0a1a7859e42577910
Pu-Junming <810102685@qq.com>
2022-12-03 11:48:56 +0800

    Site updated: 2022-12-03 11:48:48

--- js/custom.js
+++ js/custom.js
@@ -64,3 +64,8 @@ if (mobile_sidebar_menus) {
 //         }), !1
 //     }
 // }
+
+// 禁止右键及F12等事件
+document.onkeydown = function (e) {
+    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
+};
